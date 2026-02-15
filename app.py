650e5c51cb0786a7ec51028ac071664e4a4a0cf8
varshiniayyavari <varshiniayyavari4@gmail.com>
2026-02-15 12:31:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-import streamlit as st
-import pickle
-import numpy as np
-import pandas as pd
-
-# Load model, scaler, feature names
-with open("credit_model.pkl", "rb") as f:
-    model = pickle.load(f)
-scaler = pickle.load(open("scaler.pkl", "rb"))
-feature_names = pickle.load(open("feature_names.pkl", "rb"))
-
-st.title("💳 Credit Risk Prediction App")
-
-st.write("Enter customer details below:")
-
-# User Inputs
-person_age = st.number_input("Age", min_value=18, max_value=100, value=30)
-person_income = st.number_input("Annual Income", min_value=0, value=50000)
-person_emp_length = st.number_input("Employment Length (years)", min_value=0, value=2)
-loan_amnt = st.number_input("Loan Amount", min_value=0, value=10000)
-loan_int_rate = st.number_input("Interest Rate", min_value=0.0, value=10.0)
-loan_percent_income = st.number_input("Loan % of Income", min_value=0.0, value=0.2)
-cb_person_cred_hist_length = st.number_input("Credit History Length", min_value=0, value=5)
-
 if st.button("Predict"):
-    input_data = np.array([[person_age, person_income, person_emp_length,
-                            loan_amnt, loan_int_rate,
-                            loan_percent_income,
-                            cb_person_cred_hist_length]])
 
-    input_scaled = scaler.transform(input_data)
+    # Create empty dictionary with all 22 features
+    input_dict = dict.fromkeys(feature_names, 0)
+
+    # Fill the features user actually enters
+    input_dict["person_age"] = person_age
+    input_dict["person_income"] = person_income
+    input_dict["person_emp_length"] = person_emp_length
+    input_dict["loan_amnt"] = loan_amnt
+
+    # Convert to DataFrame with correct column order
+    input_df = pd.DataFrame([input_dict])
+
+    # Scale
+    input_scaled = scaler.transform(input_df)
+
+    # Predict
     prediction = model.predict(input_scaled)
 
     if prediction[0] == 1:
